0c7b983c22b400d0a468eea3bf63f229a6c7eaef
André Santos <106824104+andresantos110@users.noreply.github.com>
2025-08-19 13:38:03 +0100

    started replacement

--- src/pp_potato.vhd
+++ src/pp_potato.vhd
@@ -29,15 +29,19 @@ entity pp_potato is
 		-- Test interface:
 		test_context_out : out test_context;
 
-		-- Wishbone interface:
-		wb_adr_out : out std_logic_vector(31 downto 0);
-		wb_sel_out : out std_logic_vector( 3 downto 0);
-		wb_cyc_out : out std_logic;
-		wb_stb_out : out std_logic;
-		wb_we_out  : out std_logic;
-		wb_dat_out : out std_logic_vector(31 downto 0);
-		wb_dat_in  : in  std_logic_vector(31 downto 0);
-		wb_ack_in  : in  std_logic;
+--		-- Wishbone interface:
+--		wb_adr_out : out std_logic_vector(31 downto 0);
+--		wb_sel_out : out std_logic_vector( 3 downto 0);
+--		wb_cyc_out : out std_logic;
+--		wb_stb_out : out std_logic;
+--		wb_we_out  : out std_logic;
+--		wb_dat_out : out std_logic_vector(31 downto 0);
+--		wb_dat_in  : in  std_logic_vector(31 downto 0);
+--		wb_ack_in  : in  std_logic;
+
+
+        -- AXI4 Interface
+        
 		
 		-- Step-by-step unit signals
 		current_pc : out std_logic_vector(31 downto 0);
@@ -62,13 +66,18 @@ architecture behaviour of pp_potato is
 	signal dmem_write_req : std_logic;
 	signal dmem_write_ack : std_logic;
 
-	-- Wishbone signals:
-	signal icache_inputs, dmem_if_inputs   : wishbone_master_inputs;
-	signal icache_outputs, dmem_if_outputs : wishbone_master_outputs;
+--	-- Wishbone signals:
+--	signal icache_inputs, dmem_if_inputs   : wishbone_master_inputs;
+--	signal icache_outputs, dmem_if_outputs : wishbone_master_outputs;
 
-    -- Arbiter signals:
-	signal m1_inputs, m2_inputs   : wishbone_master_inputs;
-	signal m1_outputs, m2_outputs : wishbone_master_outputs;
+--    -- Arbiter signals:
+--	signal m1_inputs, m2_inputs   : wishbone_master_inputs;
+--	signal m1_outputs, m2_outputs : wishbone_master_outputs;
+
+	-- AXI4 signals:
+	signal icache_inputs, dmem_if_inputs   : axi4lite_master_inputs;
+	signal icache_outputs, dmem_if_outputs : axi4lite_master_outputs;
+
 
 begin
 
@@ -98,57 +107,91 @@ begin
 
     current_pc <= imem_address;
     
-	icache_enabled: if ICACHE_ENABLE
-	generate
-		icache: entity work.pp_icache
-			generic map(
-				LINE_SIZE => ICACHE_LINE_SIZE,
-				NUM_LINES => ICACHE_NUM_LINES
-			) port map(
-				clk => clk,
-				reset => reset,
-				mem_address_in => imem_address,
-				mem_data_out => imem_data,
-				mem_read_req => imem_req,
-				mem_read_ack => imem_ack,
-				wb_inputs => icache_inputs,
-				wb_outputs => icache_outputs
-			);
+--	icache_enabled: if ICACHE_ENABLE
+--	generate
+--		icache: entity work.pp_icache
+--			generic map(
+--				LINE_SIZE => ICACHE_LINE_SIZE,
+--				NUM_LINES => ICACHE_NUM_LINES
+--			) port map(
+--				clk => clk,
+--				reset => reset,
+--				mem_address_in => imem_address,
+--				mem_data_out => imem_data,
+--				mem_read_req => imem_req,
+--				mem_read_ack => imem_ack,
+--				wb_inputs => icache_inputs,
+--				wb_outputs => icache_outputs
+--			);
 
-		icache_inputs <= m1_inputs;
-		m1_outputs <= icache_outputs;
+--		icache_inputs <= m1_inputs;
+--		m1_outputs <= icache_outputs;
 
-		dmem_if_inputs <= m2_inputs;
-		m2_outputs <= dmem_if_outputs;
-	end generate icache_enabled;
+--		dmem_if_inputs <= m2_inputs;
+--		m2_outputs <= dmem_if_outputs;
+--	end generate icache_enabled;
 
-	icache_disabled: if not ICACHE_ENABLE
-	generate
-		imem_if: entity work.pp_wb_adapter
-			port map(
-				clk => clk,
-				reset => reset,
-				mem_address => imem_address,
-				mem_data_in => (others => '0'),
-				mem_data_out => imem_data,
-				mem_data_size => (others => '0'),
-				mem_read_req => imem_req,
-				mem_read_ack => imem_ack,
-				mem_write_req => '0',
-				mem_write_ack => open,
-				wb_inputs => icache_inputs,
-				wb_outputs => icache_outputs
-			);
+--	icache_disabled: if not ICACHE_ENABLE
+--	generate
+--		imem_if: entity work.pp_wb_adapter
+--			port map(
+--				clk => clk,
+--				reset => reset,
+--				mem_address => imem_address,
+--				mem_data_in => (others => '0'),
+--				mem_data_out => imem_data,
+--				mem_data_size => (others => '0'),
+--				mem_read_req => imem_req,
+--				mem_read_ack => imem_ack,
+--				mem_write_req => '0',
+--				mem_write_ack => open,
+--				wb_inputs => icache_inputs,
+--				wb_outputs => icache_outputs
+--			);
 
-		dmem_if_inputs <= m1_inputs;
-		m1_outputs <= dmem_if_outputs;
+--		dmem_if_inputs <= m1_inputs;
+--		m1_outputs <= dmem_if_outputs;
 
-		icache_inputs <= m2_inputs;
-		m2_outputs <= icache_outputs;
-	end generate icache_disabled;
+--		icache_inputs <= m2_inputs;
+--		m2_outputs <= icache_outputs;
+--	end generate icache_disabled;
 
-	dmem_if: entity work.pp_wb_adapter
-		port map(
+--	dmem_if: entity work.pp_wb_adapter
+--		port map(
+--			clk => clk,
+--			reset => reset,
+--			mem_address => dmem_address,
+--			mem_data_in => dmem_data_out,
+--			mem_data_out => dmem_data_in,
+--			mem_data_size => dmem_data_size,
+--			mem_read_req => dmem_read_req,
+--			mem_read_ack => dmem_read_ack,
+--			mem_write_req => dmem_write_req,
+--			mem_write_ack => dmem_write_ack,
+--			wb_inputs => dmem_if_inputs,
+--			wb_outputs => dmem_if_outputs
+--		);
+
+--	arbiter: entity work.pp_wb_arbiter
+--		port map(
+--			clk => clk,
+--			reset => reset,
+--			m1_inputs => m1_inputs,
+--			m1_outputs => m1_outputs,
+--			m2_inputs => m2_inputs,
+--			m2_outputs => m2_outputs,
+--			wb_adr_out => wb_adr_out,
+--			wb_sel_out => wb_sel_out,
+--			wb_cyc_out => wb_cyc_out,
+--			wb_stb_out => wb_stb_out,
+--			wb_we_out => wb_we_out,
+--			wb_dat_out => wb_dat_out,
+--			wb_dat_in => wb_dat_in,
+--			wb_ack_in => wb_ack_in
+--		);
+
+    imem_if: entity work.pp_axi4_adapter
+    		port map(
 			clk => clk,
 			reset => reset,
 			mem_address => dmem_address,
@@ -159,26 +202,39 @@ begin
 			mem_read_ack => dmem_read_ack,
 			mem_write_req => dmem_write_req,
 			mem_write_ack => dmem_write_ack,
-			wb_inputs => dmem_if_inputs,
-			wb_outputs => dmem_if_outputs
+			axi_inputs => icache_inputs,
+			axi_outputs => icache_outputs
 		);
 
-	arbiter: entity work.pp_wb_arbiter
-		port map(
+    dmem_if: entity work.pp_axi4_adapter
+    		port map(
 			clk => clk,
 			reset => reset,
-			m1_inputs => m1_inputs,
-			m1_outputs => m1_outputs,
-			m2_inputs => m2_inputs,
-			m2_outputs => m2_outputs,
-			wb_adr_out => wb_adr_out,
-			wb_sel_out => wb_sel_out,
-			wb_cyc_out => wb_cyc_out,
-			wb_stb_out => wb_stb_out,
-			wb_we_out => wb_we_out,
-			wb_dat_out => wb_dat_out,
-			wb_dat_in => wb_dat_in,
-			wb_ack_in => wb_ack_in
+			mem_address => dmem_address,
+			mem_data_in => dmem_data_out,
+			mem_data_out => dmem_data_in,
+			mem_data_size => dmem_data_size,
+			mem_read_req => dmem_read_req,
+			mem_read_ack => dmem_read_ack,
+			mem_write_req => dmem_write_req,
+			mem_write_ack => dmem_write_ack,
+			axi_inputs => dmem_if_inputs,
+			axi_outputs => dmem_if_outputs
 		);
+		
+	imem_wrapper: entity work.pp_axi_wrapper
+	       port map(
+	       
+	       
+	    );
+	    
+	dmem_wrapper: entity work.pp_axi_wrapper
+	       port map(
+	       
+	       
+	    );
+		
+		
+		
 
 end architecture behaviour;
