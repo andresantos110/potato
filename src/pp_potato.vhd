2e771366d1b27b619b257da7e0585d7082503828
André Santos <106824104+andresantos110@users.noreply.github.com>
2025-10-18 16:13:03 +0100

    Update pp_potato.vhd

--- src/pp_potato.vhd
+++ src/pp_potato.vhd
@@ -111,10 +111,6 @@ architecture behaviour of pp_potato is
 --	signal m1_inputs, m2_inputs   : wishbone_master_inputs;
 --	signal m1_outputs, m2_outputs : wishbone_master_outputs;
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 begin
 
 	processor: entity work.pp_core
@@ -239,7 +235,6 @@ begin
 			mem_write_req => dmem_write_req,
 			mem_write_ack => dmem_write_ack,
 			M_AXI_AWADDR => I_AXI_AWADDR,
-<<<<<<< Updated upstream
 			M_AXI_AWVALID => I_AXI_AWVALID,
 			M_AXI_AWREADY => I_AXI_AWREADY,
 			M_AXI_WDATA => I_AXI_WDATA,
@@ -256,24 +251,6 @@ begin
 			M_AXI_RRESP => I_AXI_RRESP,
 			M_AXI_RVALID => I_AXI_RVALID,
 			M_AXI_RREADY => I_AXI_RREADY 
-=======
-            M_AXI_AWVALID => I_AXI_AWVALID,
-            M_AXI_AWREADY => I_AXI_AWREADY,
-            M_AXI_WDATA => I_AXI_WDATA,
-            M_AXI_WSTRB => I_AXI_WSTRB,
-            M_AXI_WVALID => I_AXI_WVALID,
-            M_AXI_WREADY => I_AXI_WREADY,
-            M_AXI_BRESP => I_AXI_BRESP,
-            M_AXI_BVALID => I_AXI_BVALID,
-            M_AXI_BREADY => I_AXI_BREADY,
-            M_AXI_ARADDR => I_AXI_ARADDR,
-            M_AXI_ARVALID => I_AXI_ARVALID,
-            M_AXI_ARREADY => I_AXI_ARREADY,
-            M_AXI_RDATA => I_AXI_RDATA,
-            M_AXI_RRESP => I_AXI_RRESP,
-            M_AXI_RVALID => I_AXI_RVALID,
-            M_AXI_RREADY => I_AXI_RREADY  
->>>>>>> Stashed changes
 		);
 
     dmem_if: entity work.pp_axi4_adapter
@@ -289,7 +266,6 @@ begin
 			mem_write_req => dmem_write_req,
 			mem_write_ack => dmem_write_ack,
 			M_AXI_AWADDR => D_AXI_AWADDR,
-<<<<<<< Updated upstream
 			M_AXI_AWVALID => D_AXI_AWVALID,
 			M_AXI_AWREADY => D_AXI_AWREADY,
 			M_AXI_WDATA => D_AXI_WDATA,
@@ -308,24 +284,5 @@ begin
 			M_AXI_RREADY => D_AXI_RREADY 
 		);
 		
-=======
-            M_AXI_AWVALID => D_AXI_AWVALID,
-            M_AXI_AWREADY => D_AXI_AWREADY,
-            M_AXI_WDATA => D_AXI_WDATA,
-            M_AXI_WSTRB => D_AXI_WSTRB,
-            M_AXI_WVALID => D_AXI_WVALID,
-            M_AXI_WREADY => D_AXI_WREADY,
-            M_AXI_BRESP => D_AXI_BRESP,
-            M_AXI_BVALID => D_AXI_BVALID,
-            M_AXI_BREADY => D_AXI_BREADY,
-            M_AXI_ARADDR => D_AXI_ARADDR,
-            M_AXI_ARVALID => D_AXI_ARVALID,
-            M_AXI_ARREADY => D_AXI_ARREADY,
-            M_AXI_RDATA => D_AXI_RDATA,
-            M_AXI_RRESP => D_AXI_RRESP,
-            M_AXI_RVALID => D_AXI_RVALID,
-            M_AXI_RREADY => D_AXI_RREADY   
-		);
->>>>>>> Stashed changes
 		
 end architecture behaviour;
